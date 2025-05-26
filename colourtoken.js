bbc4eab7481adac0b1718c4e72b68a936e98f854
Lương Quốc Minh <89959828+Otisgray505@users.noreply.github.com>
2025-05-26 05:02:25 -0700

    Add files via upload

---
+++ colourtoken.js
@@ -0,0 +1,105 @@
+[
+  {
+    "hex": "#ebedf7",
+    "rgb": "rgb(235, 237, 247)",
+    "token": "blueLight"
+  },
+  {
+    "hex": "#e1e4f2",
+    "rgb": "rgb(225, 228, 242)",
+    "token": "blueLightHover"
+  },
+  {
+    "hex": "#c1c8e5",
+    "rgb": "rgb(193, 200, 229)",
+    "token": "blueLightActive"
+  },
+  {
+    "hex": "#364daa",
+    "rgb": "rgb(54, 77, 170)",
+    "token": "blueNormal"
+  },
+  {
+    "hex": "#314599",
+    "rgb": "rgb(49, 69, 153)",
+    "token": "blueNormalHover"
+  },
+  {
+    "hex": "#2b3e88",
+    "rgb": "rgb(43, 62, 136)",
+    "token": "blueNormalActive"
+  },
+  {
+    "hex": "#293a80",
+    "rgb": "rgb(41, 58, 128)",
+    "token": "blueDark"
+  },
+  {
+    "hex": "#202e66",
+    "rgb": "rgb(32, 46, 102)",
+    "token": "blueDarkHover"
+  },
+  {
+    "hex": "#18234c",
+    "rgb": "rgb(24, 35, 76)",
+    "token": "blueDarkActive"
+  },
+  {
+    "hex": "#131b3b",
+    "rgb": "rgb(19, 27, 59)",
+    "token": "blueDarker"
+  }
+]
+
+[
+  {
+    "hex": "#f4e9ee",
+    "rgb": "rgb(244, 233, 238)",
+    "token": "redLight"
+  },
+  {
+    "hex": "#eedee6",
+    "rgb": "rgb(238, 222, 230)",
+    "token": "redLightHover"
+  },
+  {
+    "hex": "#dcbbcb",
+    "rgb": "rgb(220, 187, 203)",
+    "token": "redLightActive"
+  },
+  {
+    "hex": "#8f2456",
+    "rgb": "rgb(143, 36, 86)",
+    "token": "redNormal"
+  },
+  {
+    "hex": "#81204d",
+    "rgb": "rgb(129, 32, 77)",
+    "token": "redNormalHover"
+  },
+  {
+    "hex": "#721d45",
+    "rgb": "rgb(114, 29, 69)",
+    "token": "redNormalActive"
+  },
+  {
+    "hex": "#6b1b41",
+    "rgb": "rgb(107, 27, 65)",
+    "token": "redDark"
+  },
+  {
+    "hex": "#561634",
+    "rgb": "rgb(86, 22, 52)",
+    "token": "redDarkHover"
+  },
+  {
+    "hex": "#401027",
+    "rgb": "rgb(64, 16, 39)",
+    "token": "redDarkActive"
+  },
+  {
+    "hex": "#320d1e",
+    "rgb": "rgb(50, 13, 30)",
+    "token": "redDarker"
+  }
+]
